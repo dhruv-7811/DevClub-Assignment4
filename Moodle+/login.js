52ed6bf418096478b685c8b9663d62076bc618ba
dhruv <dhruv7811@gmail.com>
2022-03-19 15:16:19 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,23 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector("#login"); // Fill the selector for the login element in ""
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let question = login_text.split(' ');
+
 
 let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
-
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+if (question[5]=="add"){
+  answer= String(question[6]+question[8])  
+} 
+else if (question[5]=="subtract"){
+    answer= String(question[6]-question[8])  
+}
+else if (question[5]=="enter"){
+    if (question[6]=="first"){
+        answer=String(question[8])
+    } 
+    else
+        answer=String(question[10])
+}
+const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
 captcha_input_element.value = answer;
